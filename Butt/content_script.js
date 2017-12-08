e2f347f8266a5e8241908d06648f980a5bf33155
viperman1271 <viperman1271@hotmail.com>
2017-12-07 22:14:08 -0500

    Added Butt plugin example

---
+++ Butt/content_script.js
@@ -0,0 +1,46 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+	
+	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	    || node.classList.indexOf('ace_editor') > -1) {
+		return;
+	}
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bThe Butt\b/g, "My Butt");
+	v = v.replace(/\bThe butt\b/g, "My butt");
+	v = v.replace(/\bthe Butt\b/g, "my Butt");
+	v = v.replace(/\bthe butt\b/g, "my butt");
+	
+	textNode.nodeValue = v;
+}
+
