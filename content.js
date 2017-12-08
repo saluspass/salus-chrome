480006d7cc50e2d96a5ef3f9ce08962280f7b9b3
viperman1271 <viperman1271@hotmail.com>
2017-12-07 22:12:37 -0500

    Adding basic prototype

---
+++ content.js
@@ -0,0 +1,27 @@
+function isAccount(id) {
+    id = id.toLowerCase();
+    return id.includes("username") || id.includes("email");
+}
+
+function replacePassword(username, password){
+    var inputs = document.getElementsByTagName('input');
+    for (var i = 0, l = inputs.length; i < l; i++) {
+        if(inputs[i].type == "password") {
+            inputs[i].value = password;
+        }
+        else if(inputs[i].type == "text" && isAccount(inputs[i].id)) {
+            inputs[i].value = username;
+        }
+    }
+}
+
+var getUrl = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l.hostname;
+};
+
+var tabURL = window.location.href;
+console.log(getUrl(tabURL));
+
+replacePassword("username", "password");
